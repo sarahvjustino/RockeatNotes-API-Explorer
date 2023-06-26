bae75de2b65d65bd9daf39e92646d4fa486b69f2
Sarah <sarahvictoria230@gmail.com>
2023-06-26 14:39:59 -0300

    Changing password to hashedPassword

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -1,5 +1,5 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
-
 const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
@@ -13,10 +13,11 @@ class UsersController {
             throw new AppError("Esse e-mail já está em uso.");
         }
 
+        const hashedPassword = await hash(password, 8);
 
         await database.run(
             "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-            [name, email, password]
+            [name, email, hashedPassword]
         )
 
         return response.status(201).json();
